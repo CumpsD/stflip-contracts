f8edd37870d5990a8d3d823842724a44213293fe
Addison <captainadd49@gmail.com>
2023-11-04 11:07:31 -0400

    frax deploys

---
+++ script/deploy/DeployFraxGuard.s.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: ISC
+pragma solidity ^0.8.19;
+
+import { BaseScript } from "frax-std/BaseScript.sol";
+import { console } from "frax-std/FraxTest.sol";
+import { Constants } from "@script-deploy/Constants.sol";
+import { FraxGuard } from "@governance/src/FraxGuard.sol";
+
+function deployFraxGuard(
+    address _fraxGovernorOmega
+) returns (address _address, bytes memory _constructorParams, string memory _contractName) {
+    _constructorParams = abi.encode(_fraxGovernorOmega);
+    _contractName = "FraxGuard";
+    _address = address(new FraxGuard(_fraxGovernorOmega));
+}
+
+contract DeployFraxGuard is BaseScript {
+    function run()
+        external
+        broadcaster
+        returns (address _address, bytes memory _constructorParams, string memory _contractName)
+    {
+        (_address, _constructorParams, _contractName) = deployFraxGuard(Constants.FRAX_GOVERNOR_OMEGA);
+    }
+}
